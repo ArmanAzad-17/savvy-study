4f8a555c50ca3bd151aa27e007442c7f48314392
arman <armanazad.sakib@gmail.com>
2022-02-25 18:10:31 +0600

    grapql app sync complete

--- app/graphql.js
+++ app/graphql.js
@@ -1,7 +1,24 @@
 'use strict';
+exports.handler = async (event,context,callback) => {
+  let responses = '';
 
-exports.handler = async (event) => {
-  return {
-    data: 'Hello serverless appsync graphql world'
-  };
+  switch (event.info.fieldName) {
+    case 'getMessage':
+      responses = {data:'Well comr serverless appsync graphql !'}
+      break;
+    case 'listUsers':
+        responses = [{
+          user_id: '31f41a41-14f2-4209-bdf3-c2739b5a63fd',
+          user_email: 'armanazad.sakib@gmail.com',
+          user_name: 'Arman azad'
+        }];
+      break;
+    default:
+      responses = {data:'Sorry no query found'}
+      break;
+  }
+
+  console.log(responses);
+
+  return responses;
 };
